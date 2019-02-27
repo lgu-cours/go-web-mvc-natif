4b278411f13acd590f2a0b15c9ff5d1d11082482
l-gu <laurent.guer1@gmail.com>
2019-02-27 11:00:09 +0100

    currentDir + filePath added

--- src/step00/main0.go
+++ src/step00/main0.go
@@ -1,9 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+    "os"
+    "path/filepath"	
+)
+func currentDir() {
+    // Current directory
+    ex, err := os.Executable()
+    if err != nil {
+        panic(err)
+    }
+    exPath := filepath.Dir(ex)
+    fmt.Println("Current directory : ")
+    fmt.Println(exPath)	
+}
+
+func absoluteFilePath(fileName string) {
+    fmt.Println("Absolute file path for '" + fileName + "' :")
+    s, _ := filepath.Abs(fileName)
+	fmt.Println(s)
+}
 
 func main() {
     fmt.Println("hello world")
-    
-    
+    fmt.Println("")
+    currentDir()
+    fmt.Println("")
+    absoluteFilePath("foo.txt")
 }
